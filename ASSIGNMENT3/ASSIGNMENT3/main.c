f6c6494890f8d6903874581950c295ea0421935d
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2024-10-30 12:08:55 -0400

    player2 win added

---
+++ ASSIGNMENT3/ASSIGNMENT3/main.c
@@ -0,0 +1,51 @@
+// CSC71020 - 2024-10-30 - JACKSON GLOVER - ASSIGNMENT 3
+
+#include <string.h>
+#include <stdio.h>
+
+#define MAX_CHOICE_SIZE 9
+
+char* rps(char player1Choice[MAX_CHOICE_SIZE], char* player2Choice[MAX_CHOICE_SIZE]) {
+
+	// **
+	// Converts the String inputs to integer values
+	// Stores them into values array
+	char strings[2][10] = { 0 };
+	int values[2] = { 0 };
+
+	strcpy_s(strings[0], MAX_CHOICE_SIZE, player1Choice);
+	strcpy_s(strings[1], MAX_CHOICE_SIZE, player2Choice);
+
+	for (int i = 0; i < 2; i++) {
+		if (0 == strcmp("Rock", strings[i])) {
+			values[i] = 1;
+		}
+		else if (0 == strcmp("Scissors", strings[i])) {
+			values[i] = 2;
+		}
+		else if (0 == strcmp("Paper", strings[i])) {
+			values[i] = 3;
+		}
+	}
+	// **
+
+	// handles draws
+	if (values[0] == values[1] && 0 != values[0]) {
+		return "Draw";
+	}
+	// handles player 1 victory
+	else if (values[0] == values[1] - 1 || (3 == values[0] && 1 == values[1])) {
+		return "Player1";
+	}
+	// handles player 2 victory
+	else if (values[1] == values[0] - 1 || (3 == values[1] && 1 == values[0])) {
+		return "Player2";
+	}
+
+	return "Invalid";
+}
+
+
+int main(void) {
+	return 0;
+}
---
+++ ASSIGNMENT3/RPS_Test/RPS_Test.cpp
@@ -0,0 +1,57 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include <string.h>
+#include <stdbool.h>
+
+extern "C" char* rps(char* player1Choice, char* player2Choice);
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RPSTest
+{
+	TEST_CLASS(RPSTest)
+	{
+	public:
+		
+		TEST_METHOD(INVALID_INPUT_IS_ENTERED)
+		{
+			Assert::AreEqual(0, strcmp("Invalid", rps("rock", "xx")));
+		}
+
+		TEST_METHOD(ROCK_DRAW) {
+			Assert::AreEqual(0, strcmp("Draw", rps("Rock", "Rock")));
+		}
+
+		TEST_METHOD(PAPER_DRAW) {
+			Assert::AreEqual(0, strcmp("Draw", rps("Paper", "Paper")));
+		}
+
+		TEST_METHOD(SCISSORS_DRAW) {
+			Assert::AreEqual(0, strcmp("Draw", rps("Scissors", "Scissors")));
+		}
+
+		TEST_METHOD(ROCK_SCISSORS) {
+			Assert::AreEqual(0, strcmp("Player1", rps("Rock", "Scissors")));
+		}
+
+		TEST_METHOD(SCISSORS_PAPER) {
+			Assert::AreEqual(0, strcmp("Player1", rps("Scissors", "Paper")));
+		}
+
+		TEST_METHOD(PAPER_ROCK) {
+			Assert::AreEqual(0, strcmp("Player1", rps("Paper", "Rock")));
+		}
+
+		TEST_METHOD(ROCK_PAPER) {
+			Assert::AreEqual(0, strcmp("Player2", rps("Rock", "Paper")));
+		}
+
+		TEST_METHOD(PAPER_SCISSORS) {
+			Assert::AreEqual(0, strcmp("Player2", rps("Paper", "Scissors")));
+		}
+
+		TEST_METHOD(SCISSORS_ROCK) {
+			Assert::AreEqual(0, strcmp("Player2", rps("Scissors", "Rock")));
+		}
+	};
+}
